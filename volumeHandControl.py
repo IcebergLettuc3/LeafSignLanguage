9ba098c80f628a8a97d364361ce4d41ee12fc59f
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-12-27 23:59:50 -0600

    adding fps counter

--- volumeHandControl.py
+++ volumeHandControl.py
@@ -15,6 +15,8 @@ while True:
   cTime = time.time()
   fps = 1/(cTime - pTime)
   pTime = cTime
+
+  cv2.putText(img, f'FPS: {int(fps)}', (20, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2 )
   
   cv2.imshow("Img", img)
   cv2.waitKey(1)
