df9db39af182bc825d16437708cdd71361bc61b0
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-12-28 00:46:56 -0600

    adding highlighting of specific nodes

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -22,8 +22,10 @@ while True:
       for id, lm in enumerate(handLms.landmark):
         # print(id, lm)
         h, w, c = img.shape
-        cx, cy = int(lm,x*w), int(lm.y*h)
+        cx, cy = int(lm.x*w), int(lm.y*h)
         print(id, cx, cy)
+        if id == 12:
+          cv2.circle(img, (cx, cy), 20, (255, 0, 255), cv2.FILLED)
 
       mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
