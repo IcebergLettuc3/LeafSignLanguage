6c761274ed6089c50442c8adce183506c487e43c
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-12-28 01:14:04 -0600

    creating find hands function

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -5,28 +5,35 @@ import time
 class handDetector():
   def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon = 0.5):
     self.mode = mode
+    self.maxHands = maxHands
+    self.detectionCon = detectionCon
+    self.trackCon = trackCon
+    
+    self.mpHands = mp.solutions.hands
+    self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+    self.mpDraw = mp.solutions.drawing_utils
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+  def findHands(self, img):
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = self.hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+      for handLms in results.multi_hand_landmarks:
+        for id, lm in enumerate(handLms.landmark):
+          # print(id, lm)
+          h, w, c = img.shape
+          cx, cy = int(lm.x*w), int(lm.y*h)
+          # print(id, cx, cy)
+          if id == 12:
+            cv2.circle(img, (cx, cy), 20, (255, 0, 255), cv2.FILLED) #this line highlights the tip of the middle finger
+
+        self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
 
 
-  # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  # results = hands.process(imgRGB)
-  # # print(results.multi_hand_landmarks)
 
-  # if results.multi_hand_landmarks:
-  #   for handLms in results.multi_hand_landmarks:
-  #     for id, lm in enumerate(handLms.landmark):
-  #       # print(id, lm)
-  #       h, w, c = img.shape
-  #       cx, cy = int(lm.x*w), int(lm.y*h)
-  #       # print(id, cx, cy)
-  #       if id == 12:
-  #         cv2.circle(img, (cx, cy), 20, (255, 0, 255), cv2.FILLED) #this line highlights the tip of the middle finger
 
-  #     mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
   
   
