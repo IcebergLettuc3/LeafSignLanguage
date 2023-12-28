46bade8fb42b39b699189221c30220a90a322125
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-12-27 23:25:46 -0600

    adding fps timer

--- volumeHandControl.py
+++ volumeHandControl.py
@@ -7,11 +7,14 @@ wCam, hCam = 1280, 720
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
-
-
+pTime = 0
 
 while True:
   success, img = cap.read()
+
+  cTime = time.time()
+  fps = 1/(cTime - pTime)
+  pTime = cTime
   
   cv2.imshow("Img", img)
   cv2.waitKey(1)
