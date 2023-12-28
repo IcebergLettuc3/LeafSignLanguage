96ed7df6819572489ad63e87c4b5bfd5d168e100
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-12-28 00:58:47 -0600

    creating hand tracking module

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -8,11 +8,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-pTime = 0
-cTime = 0
 
-while True:
-  success, img = cap.read()
+
   imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
   results = hands.process(imgRGB)
   # print(results.multi_hand_landmarks)
@@ -29,10 +26,22 @@ while True:
 
       mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-  cTime = time.time()
-  fps = 1/(cTime - pTime)
-  pTime = cTime
-  cv2.putText(img, str(int(fps)), (20, 40), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 32, 49), 2)
+  
+  
 
-  cv2.imshow("Image", img)
-  cv2.waitKey(1)
+def main():
+  pTime = 0
+  cTime = 0
+  while True:
+    success, img = cap.read()
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 40), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 32, 49), 2)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+
+if __name__ == "__main__":
