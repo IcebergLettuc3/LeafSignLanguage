43a3f4548ed3330d9b2e577d80c4c2b1974ef958
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-12-28 03:41:28 -0600

    drawing on hand

--- volumeHandControl.py
+++ volumeHandControl.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
+import math
 
 wCam, hCam = 1280, 720
 
@@ -17,7 +18,16 @@ while True:
   img = detector.findHands(img)
   lmList = detector.findPosition(img, draw=False)
   if lmList:
-    print(lmList[2])
+    # print(lmList[2])
+    x1, y1 = lmList[4][1], lmList[4][2]
+    x2, y2 = lmList[8][1], lmList[8][2]
+    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+    cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+    cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
 
   cTime = time.time()
   fps = 1/(cTime - pTime)
